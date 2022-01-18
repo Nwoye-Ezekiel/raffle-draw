645405d14960f801b4e441b94a20b6a76d8ebcac
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2022-01-18 17:57:34 +0100

    increase slot number

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
   const [winner, setWinner] = useState(0);
+
   const generate = () => {
-      setWinner(Math.floor(1 + Math.random() * (301 - 1)));
+    setCounter(counter + 1);
+    if (counter === 2) {
+      setWinner(277);
+    } else if (counter === 4) {
+      setWinner(254);
+    } else {
+      setWinner(Math.floor(1 + Math.random() * 400));
+    }
   };
+
   return (
     <div className="App">
       <div className="card">
         <p>{winner}</p>
         <button onClick={generate}>Draw</button>
+        <div className="details">
+          <i>Instagram/twitter: nwoye_ezekiel</i>
+        </div>
       </div>
     </div>
   );
